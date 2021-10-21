462af3728bde1841534ac4a9655066b0bf17a577
AldoBertDulanto <aldobertdulanto@hotmail.com>
2021-10-21 18:12:28 -0500

    Last One

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -15,10 +15,10 @@ document.addEventListener("DOMContentLoaded",
 (function (global){
     var dc = {};
     var homeHtmlUrl = "snippets/home-snippet.html";
-    var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+    var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
-    var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemsHtml = "snippets/menu-item.html";
 
